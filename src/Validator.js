7fe793868a02be4fef9f171bc80538ee08298255
AndrewNikitin127 <donandrain153@gmail.com>
2023-12-25 16:21:03 +0500

    first task complete

--- src/Validator.js
+++ src/Validator.js
@@ -1,1 +1,16 @@
+import StringSchema from './StringSchema.js'
 
+export default class Validator {
+  string(){
+    return new StringSchema();
+  }
+  number(){
+    return new NumberSchema();
+  }
+  array(){
+    return new ArraySchema();
+  }
+  object(){
+    return new ObjectSchema();
+  }
+}
