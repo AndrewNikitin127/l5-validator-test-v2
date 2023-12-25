fcef1998cb4380178ba5b9ba3ec9d93ace8e2a50
AndrewNikitin127 <donandrain153@gmail.com>
2023-12-25 17:12:16 +0500

    fourth task complete

--- src/ArraySchema.js
+++ src/ArraySchema.js
@@ -8,5 +8,18 @@ export default class ArraySchema{
   isValid(val){
     return this.validators.every(validator => validator(val));
   }
-  
+  maxDepth(maxDepth){
+    const validator = (array) => {
+      
+      const getDepth = (arr, depth = 0) => {
+        const arrays = arr.filter(e => Array.isArray(e));
+        if (arrays.length === 0) return depth;
+
+        const arraysDepth = arrays.map( e => getDepth(e, depth + 1))
+        return Math.max(...arraysDepth);
+      }
+      return getDepth(array) <= maxDepth;
+    }
+    return new ArraySchema([...this.validators, validator])
+  }
 }
